c47211cf7ef0e97fbccc377a3453f075a196c9c4
elspirito <el_spirito@bk.ru>
2023-11-11 11:49:10 +0300

    hw9 make Day

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -37,7 +37,10 @@ function Clock() {
     const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = date.getDay() || <br/> // пишут студенты
+    let formatter = new Intl.DateTimeFormat("en", {
+        weekday: "long",
+    });
+    const stringDay = `${formatter.format(date)}` || <br/> // пишут студенты
     const stringMonth = 'date->month' || <br/> // пишут студенты
 
     return (
