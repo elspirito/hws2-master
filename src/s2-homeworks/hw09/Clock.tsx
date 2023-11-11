50af0098a53e4e6cb93d32f2e112ca8fc969b187
elspirito <el_spirito@bk.ru>
2023-11-11 11:58:45 +0300

    hw9 make Month

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -27,21 +27,24 @@ function Clock() {
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
+        setShow(true)
     }
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
+        setShow(false)
     }
 
     const stringTime = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    let formatter = new Intl.DateTimeFormat("en", {
+    let formatterDay = new Intl.DateTimeFormat("en", {
         weekday: "long",
     });
-    const stringDay = `${formatter.format(date)}` || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    let formatterMonth = new Intl.DateTimeFormat("en", {
+        month: "long",
+    });
+    const stringDay = `${formatterDay.format(date)}` || <br/> // пишут студенты
+    const stringMonth = `${formatterMonth.format(date)}` || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
